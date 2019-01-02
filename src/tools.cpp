20b2baa774cecad2350936f99ca0ca4d3ed924df
creich <creich@linux.com>
2019-01-02 10:51:04 +0100

    fixed float error in CalculateJacobian()

--- src/tools.cpp
+++ src/tools.cpp
@@ -51,11 +51,6 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
-
    MatrixXd Hj(3,4);
    // recover state parameters
    float px = x_state(0);
@@ -63,17 +58,19 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    float vx = x_state(2);
    float vy = x_state(3);
 
-   // check division by zero
-   if( (px == 0) && (py == 0)) {
-      std::cout << "ERROR: possible division by 0! returning initial Hj" << std::endl;
-      return Hj;
-   }
-
    // compute the Jacobian matrix
    float px2py2 = px*px + py*py;
+   float s_px2py2 = sqrt(px2py2);
 
-   Hj(0, 0) = px / sqrt(px2py2);
-   Hj(0, 1) = py / sqrt(px2py2);
+   //check division by zero
+   if( px2py2 < .00001 ) {
+      px += .001;
+      py += .001;
+      px2py2 = px*px + py*py;
+   }
+
+   Hj(0, 0) = px / s_px2py2;
+   Hj(0, 1) = py / s_px2py2;
    Hj(0, 2) = 0;
    Hj(0, 3) = 0;
 
@@ -82,10 +79,11 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    Hj(1, 2) = 0;
    Hj(1, 3) = 0;
 
-   Hj(2, 0) = (py*(vx*py - vy*px)) / pow(px2py2, 3/2);
-   Hj(2, 1) = (px*(vy*px - vx*py)) / pow(px2py2, 3/2);
-   Hj(2, 2) = px / sqrt(px2py2);
-   Hj(2, 3) = py / sqrt(px2py2);
+   Hj(2, 0) = (py*(vx*py - vy*px)) / pow(px2py2, 3./2);
+   Hj(2, 1) = (px*(vy*px - vx*py)) / pow(px2py2, 3./2);
+   Hj(2, 2) = px / s_px2py2;
+   Hj(2, 3) = py / s_px2py2;
 
    return Hj;
 }
+
